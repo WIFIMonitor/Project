c7015a31b6c8a79632876effa4fd713853feb71a
Diogo <diogo.fernandes77@ua.pt>
2021-05-12 18:15:04 +0100

    query fix

--- project/WifiMonitor/views.py
+++ project/WifiMonitor/views.py
@@ -28,24 +28,23 @@ def population_building_graph(request):
 
 
 def get_buildings_count():
+    try:
+        #query para obter os n de pessoas conectados a cada ap no intervalo entre[0-15min]. o +45min da query deve-se ao facto do now() devolver em utc
+        res = client.query("select \"building\",\"clientsCount\" from clientsCount where time >= now()-15m ").raw['series'][0]["values"]
+        #print(res)
+        count = {}
 
-    #query para obter os n de pessoas conectados a cada ap no intervalo entre[0-15min]. o +45min da query deve-se ao facto do now() devolver em utc
-    res = client.query("select \"building\",\"clientsCount\" from clientsCount where time >= now()+15m ").raw['series'][0]["values"]
-    #print(res)
-    count = {}
+        #count(chave=edificio, value=n de pessoas no edificio)
+        for sample in res:
+            building = sample[1]
+            if building not in count:
+                count[building] = int(sample[2])
+            else:
+                count[building] = count[building] + int(sample[2])
+    except:
+        print('ERROR: getting data from DB')
+        count = {}
 
-    #count(chave=edificio, value=n de pessoas no edificio)
-    for sample in res:
-        building = sample[1]
-        if building not in count:
-            count[building] = int(sample[2])
-        else:
-            count[building] = count[building] + int(sample[2])
-
-
-
-    print('ERROR: getting data from DB')
-    count = {}
     return count
 
 
