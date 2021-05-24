f3138cf88b36202d2979838a9096cbe89748f1db
tiago <tiagobarros99@ua.pt>
2021-05-24 11:14:27 +0100

    added timelapse logic and generation

--- project/WifiMonitor/views.py
+++ project/WifiMonitor/views.py
@@ -1,47 +1,92 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 from influxdb import InfluxDBClient
+import plotly.express as px
 import json
 from datetime import datetime
 from .forms import DateForm
-from .models import TimelapseModel
-
 
 client = InfluxDBClient("***REMOVED***", ***REMOVED***, "***REMOVED***", "***REMOVED***", "***REMOVED***")
-# get the last timestamp value of the database
-lastValueTS = client.query("select last(clientsCount) from clientsCount").raw['series'][0]['values']
-# Get the date of the measures being displayed
-timestamp = "Dia: "+lastValueTS[0][0].replace("T"," às ")[:-8]
 global prev_id
 prev_id = 0
+
+def get_last_ts():
+    # get the last timestamp value of the database
+    return client.query("select last(clientsCount) from clientsCount").raw['series'][0]['values'][0][0]
+
+# Get the date of the measures being displayed
+timestamp = "Dia: "+get_last_ts().replace("T"," às ")[:-8]
 
 # load ap cooordinates into memory, so that we dont 
 # have to read the excel file every time someone
 # wants to check the heatmap
-def load_people_conected():
-    # subtract from the last value, the last 15 minutes
-    sq = "select id,clientsCount from clientsCount where time >= \'"+lastValueTS[0][0]+"\'-15m"
-    # get the last 15m values, from the last value in DB, and not from now(), because CISCO PRIME can stop sending values
-    people_count = client.query(sq).raw['series'][0]["values"]
-    hash_coords = {}
-    for line in people_count:
-        hash_coords[line[1]] = line[2]
-
-    coords = []
+def load_ap_coords():
+    coords = {}
     f = open("WifiMonitor/static/fileCoords.txt","r")
-    
+
     for line in f:
         info = line.split(",")
-        dic = { "id" : info[0],
+        dic = { 
         "lat" : info[1],
         "lon" : info[2].strip('\n'),
-        "people" : hash_coords[info[0]] if info[0] in hash_coords else 0,
-        "piso" : info[3] if info[3]!="None\n" else "Não Definido"
+        "piso" : info[3] if info[3]!="None\n" else "Não Definido",
         }
-        coords.append(dic)
+        coords[info[0]] = dic
     
     f.close()
     return coords
+
+coords = load_ap_coords()
+
+def get_timelapse_dictionary(dataset,endtime,starttime,measure):
+    # query values between last measurement, minus 15 minutes
+    sq = "select id,clientsCount from clientsCount where time <=\'"+starttime+"m and time > \'" +endtime+"m" 
+    # get the last 15m values, from the last value in DB, and not from now(), because CISCO PRIME can stop sending values
+    try:
+        people_count = client.query(sq).raw['series'][0]["values"]
+    except:
+        return
+   
+    for line in people_count:
+        # only add to the timelapse points that have people conected, to not overload the array
+        if line[2] != 0:
+            if line[1] in coords:
+                dic = {"id": line[1],
+                        "lat": coords[line[1]]["lat"],
+                        "lon":coords[line[1]]["lon"],
+                        "piso":coords[line[1]]["piso"],
+                        "people":line[2],
+                        "measure":measure,
+                        }     
+                dataset.append(dic)
+            else:
+                continue
+
+def get_heatmap_dictionary(timestamp):
+    dataset = []
+    # query values between last measurement, minus 15 minutes
+    sq = "select id,clientsCount from clientsCount where time >=\'"+timestamp+"\' -15m" 
+    # get the last 15m values, from the last value in DB, and not from now(), because CISCO PRIME can stop sending values
+    try:
+        people_count = client.query(sq).raw['series'][0]["values"]
+    except:
+        return
+    for line in people_count:
+        # only add to the timelapse points that have people conected, to not overload the array
+        if line[2] != 0:
+            if line[1] in coords:
+                dic = {"id": line[1],
+                        "lat": coords[line[1]]["lat"],
+                        "lon":coords[line[1]]["lon"],
+                        "piso":coords[line[1]]["piso"],
+                        "people":line[2],
+                        }     
+                dataset.append(dic)
+            else:
+                continue
+
+    return dataset
+
  
 def index(request):
     return render(request, 'index.html')
@@ -53,17 +98,17 @@ def heatmap(request):
             start = form.cleaned_data.get('start')
             end = form.cleaned_data.get('end')
             
-            utc_time = datetime.strptime(str(start), "%Y-%m-%d")
-            epoch_time = (utc_time - datetime(1970, 1, 1)).total_seconds()
-            print(epoch_time)
-            print("start: ", start)
-            print("end: ",end)
-    
+            start_time = datetime.strptime(str(start), "%Y-%m-%d").isoformat('T')
+            end_time = datetime.strptime(str(end), "%Y-%m-%d").isoformat('T')
+            
+            #generate timelapse graph
+            generateTimelapse(start_time,end_time)
 
-    ap_coordinates = load_people_conected()
+    latestTS = get_last_ts()
+    ap_values = get_heatmap_dictionary(latestTS)
     params = {
         'api_key': 'AIzaSyA9M86-1yyuucibiNR-wh8kiboANAcUjuI',
-        'data' : json.dumps(ap_coordinates),
+        'data' : json.dumps(ap_values),
         'time' : timestamp,
         'form' : form
         }
@@ -71,9 +116,10 @@ def heatmap(request):
     return render(request, 'heatmap.html', params)
 
 def overview(request):
+    latestTS = get_last_ts()
     try:
-        people_count = client.query("select sum(clientsCount) from clientsCount where time >=\'"+lastValueTS[0][0]+"\'-15m ").raw['series'][0]["values"][0][1]
-        residentials_people_count = client.query("select sum(\"clientsCount\") from clientsCount where \"building\"= \'ra\' and time >=\'"+lastValueTS[0][0]+"\'-15m").raw['series'][0]["values"][0][1]
+        people_count = client.query("select sum(clientsCount) from clientsCount where time >=\'"+latestTS+"\'-15m ").raw['series'][0]["values"][0][1]
+        residentials_people_count = client.query("select sum(\"clientsCount\") from clientsCount where \"building\"= \'ra\' and time >=\'"+latestTS+"\'-15m").raw['series'][0]["values"][0][1]
         campus_people_count = people_count - residentials_people_count
     except:
         people_count = "null"
@@ -128,8 +174,9 @@ def specific_building(request, building=None):
     return render(request, 'specific_building.html', tparams)
 
 def line_graph(request, building = None):
+    latestTS = get_last_ts()
     if(building != None):
-        values = client.query("select mean(\"sum\")from (select sum(\"clientsCount\") from clientsCount where \"building\" = \'"+building +"\' and time >=\'"+lastValueTS[0][0]+"\'-24h GROUP BY time(15m)) group by time(1h)").raw['series'][0]["values"]
+        values = client.query("select mean(\"sum\")from (select sum(\"clientsCount\") from clientsCount where \"building\" = \'"+building +"\' and time >=\'"+latestTS+"\'-24h GROUP BY time(15m)) group by time(1h)").raw['series'][0]["values"]
     print(values)
     labels = []
     data = []
@@ -145,9 +192,10 @@ def line_graph(request, building = None):
     })
 
 def get_buildings_count():
+    latestTS = get_last_ts()
     try:
         #query para obter os n de pessoas conectados a cada ap no intervalo entre[0-15min]. o +45min da query deve-se ao facto do now() devolver em utc
-        res = client.query("select \"building\",\"clientsCount\" from clientsCount where time >= \'"+lastValueTS[0][0]+"\'-15m ").raw['series'][0]["values"]
+        res = client.query("select \"building\",\"clientsCount\" from clientsCount where time >= \'"+latestTS+"\'-15m ").raw['series'][0]["values"]
         #print(res)
         count = {}
 
@@ -175,9 +223,10 @@ def get_building_names():
     return buildings
 
 def get_building_lastday_population():
+    latestTS = get_last_ts()
     try:
         #query para obter os n de pessoas conectados a cada ap no intervalo entre[0-15min]. o +45min da query deve-se ao facto do now() devolver em utc
-        res = client.query("select \"building\",\"clientsCount\" from clientsCount where time >=\'"+lastValueTS[0][0]+"\'-15m ").raw['series'][0]["values"]
+        res = client.query("select \"building\",\"clientsCount\" from clientsCount where time >=\'"+latestTS+"\'-15m ").raw['series'][0]["values"]
         #print(res)
         count = {}
 
@@ -196,13 +245,38 @@ def get_building_lastday_population():
 
     return count
 
-def generateTimelapse(request):
-    if request.method == 'POST':
-        print("post")
-        
-        datef = request.POST['datef']
-        datet = request.POST['datet']
+def generateTimelapse(start_time,end_time):
+    dataset = []
+    latestTS = get_last_ts()
+    # Generate 50 timelaspes. In the future it will be decided by the user input
+    for x in range(1,10):
+        try:
+            # since we get values every 15 minutes, we need to now how many 15 minute measures we want
+            sub1 = str(15*x)
+            sub2 = str(15*(x-1))
+            endtime = str(latestTS) + "\' -"+sub2
+            starttime = str(latestTS) + "\' -"+sub1
 
-        print("data inicio: " ,datef," data fim: ",datet)
-    
-    return 0
+            get_timelapse_dictionary(dataset,starttime,endtime,x)
+        except Exception as e:
+            print(e)
+            continue
+
+    fig = px.density_mapbox(dataset, lat='lat', lon='lon', z='people', radius=10,animation_frame='measure',
+            center=dict(lat=40.63041451444991, lon=-8.65803098047244),
+            zoom=15,
+            mapbox_style="stamen-terrain",
+            width=550,
+            height=1100,
+            color_continuous_scale= [
+                    [0.0, "green"],
+                    [0.3, "green"],
+                    [0.5, "yellow"],
+                    [0.7, "yellow"],
+                    [0.9, "red"],
+                    [1.0, "red"]],
+            title= str(latestTS) + "- " + str((x*15)) + "m",
+            range_color=(0,30), #max and min values for heatmap
+               )
+
+    fig.write_html("WifiMonitor/static/slider.html")
